b5dc8606fff73c98c77c76bf58c1d83f46e82068
izeretetachrista.gmail.com <your_email@example.com>
2026-05-06 11:43:52 -0800

    update index.js connection fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/todoapp")
+  .connect("mongodb+srv://aip:1234@cluster0.ngn31eh.mongodb.net/Cluster0")
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
 
